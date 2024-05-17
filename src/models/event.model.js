4fdfbeac1dca571d6d4ce01597e22bc51b6c598c
Dylan Erdini <erdini.dylan@gmail.com>
2024-05-17 18:55:23 -0300

    feat: define event model

---
+++ src/models/event.model.js
@@ -0,0 +1,55 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.config";
+
+const Event = sequelize.define("Event",
+	{
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+			allowNull: false,
+			primaryKey: true
+		},
+		title: {
+			type: DataTypes.STRING(100),
+			allowNull: false
+		},
+		description: {
+			type: DataTypes.TEXT,
+			allowNull: false
+		},
+		price: {
+			type: DataTypes.DECIMAL(10, 2) // Ver este campo
+		},
+		eventDate: {
+			type: DataTypes.DATEONLY,
+			allowNull
+		},
+		eventTime: {
+			type: DataTypes.TIME,
+			allowNull: false
+		},
+		street: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		lat: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		lng: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		image: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		published: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false
+		}
+	}
+)
+
+export default Event
